384142f7280d49d7c7aad98a1f93fe938e892427
myandue <97776790+myandue@users.noreply.github.com>
2025-10-23 19:20:55 +0900

    ref: quiz level (int) -> (str)으로 변경

--- django-practice/api/quiz/models.py
+++ django-practice/api/quiz/models.py
@@ -3,10 +3,10 @@ from ..users.models import User
 
 
 class QuizHistory(models.Model):
-    class Level(models.IntegerChoices):
-        _1 = 1
-        _2 = 2
-        _3 = 3
+    class Level(models.TextChoices):
+        EASY = "EASY"
+        NORMAL = "NORMAL"
+        HARD = "HARD"
 
     class Amount(models.IntegerChoices):
         _5 = 5
@@ -15,7 +15,7 @@ class QuizHistory(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     keyword = models.CharField(max_length=255)
-    level = models.IntegerField(choices=Level.choices)
+    level = models.TextField(choices=Level.choices)
     amount = models.IntegerField(choices=Amount.choices)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
